3e55d077ba4d003984887fa162c7efa63bbcaad4
aoiwashita <65690374+aoiwashita@users.noreply.github.com>
2020-05-22 09:18:50 +0900

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -25,19 +25,58 @@ Route::post('login', 'LoginController@login_succsess');
 Route::get('after_login_top', function(){
   return view('after_login_top');
 });
-Route::post('after_login_top', function(){
-  return view('after_login_top');
-});
 
+//岩下
 Route::get('/member_withdrawal', 'MemberController@remove_member');
 Route::post('/member_withdrawal_complete', 'MemberController@delete_member');
 Route::get('/member_withdrawal_complete', function(){
-  return view('member_withdrawal_complete');
-});
+  return view('member_withdrawal_complete');})
 
-
-Route::get('document_delete', 'ResisterCatalogController@remove_document');
-Route::post('document_delete_complete', 'ResisterCatalogController@delete_document');
+Route::get('document_delete', 'RegisterCatalogController@remove_document');
+Route::post('document_delete_complete', 'RegisterCatalogController@delete_document');
 Route::get('document_delete_complete', function(){
-  return view('document_delete_complete');
+  return view('document_delete_complete');});
+
+//杉澤さん
+Route::get('/member_edit', 'MemberController@edit_member');
+Route::post('/member_edit', 'MemberController@edit_member');
+Route::get('/member_edit_confirming', 'MemberController@edit_member_check');
+Route::post('/member_edit_confirming', 'MemberController@edit_member_check');
+Route::post('/member_edit_complete', 'MemberController@update_member');
+Route::get('/document_add', 'RegisterCatalogController@add_document');
+Route::get('/document_add_confirming', 'RegisterCatalogController@add_document'); //TODO 変える
+Route::post('/document_add_confirming', 'RegisterCatalogController@add_document_check');
+Route::get('/document_add_last_confirming', 'RegisterCatalogController@add_document_check');
+Route::post('/document_add_last_confirming', 'RegisterCatalogController@add_document_last_check');
+Route::post('/document_add_complete', 'RegisterCatalogController@create_document');
+
+//岡田さん
+Route::get('/member_register','MemberController@add_member');
+Route::post('/member_register','MemberController@add_member_check');
+Route::post('/member_register_complete','MemberController@create_member');
+
+//本間さん
+//資料検索画面
+Route::get('/document_search', function () {
+    return view('document_search');
 });
+Route::post('/document_search_result','RegisterCatalogController@find_document');
+
+////資料検索結果画面
+Route::get('/document_search_result', function () {
+    return view('document_search_result');
+});
+
+//吉川さん
+Route::get('/member_search', function (){
+  return view('member_search');
+});
+Route::get('/member_search_result', function (){
+  return view('member_search_result');
+});
+Route::post('/member_search_result', 'MemberController@find_member');
+Route::get('/returns', 'RentalReturnController@index');
+Route::get('/return_complete', function (){
+  return view('return_complete');
+});
+Route::post('/return_complete', 'RentalReturnController@post');
